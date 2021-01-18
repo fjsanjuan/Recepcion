90e5de2741cd5018791666ce86a58e65b5907ce9
fsanjuan <fsanjuan@intelisis.com>
2021-01-18 10:13:51 -0600

    FIX: ordenes con/sin firma formato carta garantia

--- assets/js/custom/custom-ver_historico.js
+++ assets/js/custom/custom-ver_historico.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function() {
+$(document).ready(function() {
 
 	//variableque controlan la ruta donde se guardan las fotos de la inspeccion 
 	//en este caso para poder vizualizarlas desde el historico
@@ -186,8 +186,12 @@
 				val["am_cliente"] = (val["am_cliente"] == "null") ? "" : val["am_cliente"];
 				correo_cte=val['email_cliente'];
 				trae_firma=val['contFirma']['contadorFirma'];
+				
 				if(trae_firma ==1){
 				trae_signGrtia = val['signGrtia']['firma_renunciaGarantia'];
+				}
+				else{
+					trae_signGrtia = null;
 				}
 				//console.log(trae_signGrtia);
 
