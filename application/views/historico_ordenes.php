37f66c93de42bf5695cbfe61dfe603bb5196ccd3
Timothy <timoteopena2@gmail.com>
2021-10-18 11:13:22 -0500

    FEAT: nuevo modal para archivos adjuntos

--- application/views/historico_ordenes.php
+++ application/views/historico_ordenes.php
@@ -396,3 +396,22 @@
             </div>
     </div>
 </div>
+<!-- modal archivos adjuntos -->
+<div class="modal fade" id="modalarchivosadjuntos" tabindex="-1" role="dialog" data-backdrop="false">
+    <div class="modal-dialog modal-lg"  role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Archivos Adjuntos</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php $this->load->view("modals/archivos_adjuntos");?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+    </div>
+</div>
