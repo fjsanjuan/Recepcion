cc37f7e229355a65e0941956c454534f6c25f629
Timothy <timoteopena2@gmail.com>
2021-10-18 11:23:40 -0500

    FEAT: nuevo archivo para modal archivos adjuntos

---
+++ application/views/modals/archivos_adjuntos.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 text-center"><h3>Archivo Oasis</h3></div>
+        <div class="col-sm-12" id="archivo_oasis"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 text-center"><h3>Archivos Voz del Cliente</h3></div>
+        <div class="col-sm-12" id="archivos_voc"></div>
+    </div>
+</div>
