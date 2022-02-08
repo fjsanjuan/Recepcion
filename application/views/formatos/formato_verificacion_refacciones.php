647f2e8043fe4feb3b4174952c450ced7d935420
Peña <timoteopena2@gmail.com>
2022-02-08 16:28:50 -0600

    FIX: abrir formato pdf solo cuando existe fordStar de tecnico

--- application/views/formatos/formato_verificacion_refacciones.php
+++ application/views/formatos/formato_verificacion_refacciones.php
@@ -202,6 +202,15 @@
 	<script src="<?=base_url()?>assets/librerias/html2canvas/html2canvas.min.js"></script>
 	<title>Cotización</title>
 	</head>
+	<script>
+    site_url = "<?=site_url()?>";
+    base_url = "<?=base_url()?>";
+    var id_perfil = "<?=isset($this->session->userdata['logged_in']['perfil']) ? $this->session->userdata['logged_in']['perfil'] : 0?>";             //importante!!!  
+    var fordStar = "<?=isset($this->session->userdata['logged_in']['fordStar']) ? $this->session->userdata['logged_in']['fordStar'] : null?>";             //importante!!!  
+    if (id_perfil == 5 && !fordStar) {
+    	window.location.href = "<?=site_url('user/configurar_perfil')?>";
+    }
+</script>
 	<body>
 		<div class="container borde_general">
 			<div class="row">
